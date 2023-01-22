419a869dfae210babee10cdb7310f11eeda25e03
Evgeny <bloshenkoea@gmail.com>
2023-01-22 15:58:14 +0700

    task 13

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int number;
+    string numberStr;
+    Console.Write("Enter a number: ");
+    number = int.Parse(Console.ReadLine());
+    numberStr = number.ToString();
+    if (numberStr.Length >= 3)
+    {
+        Console.WriteLine("The third digit of the number is " + numberStr[2]);
+    }
+    else
+    {
+        Console.WriteLine("The number does not have a third digit.");
+    }
+  }
+}
